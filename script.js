4ff778f5a24b69fbbc69a8c0a137a699b3511f47
Suman Hiremath <suman.hiremath89@gmail.com>
2017-12-03 13:07:13 +0530

    Revert "initial working version"
    This reverts commit 0320883ee988d80fd171e4522a65a593eabf819f.

--- script.js
+++ script.js
@@ -1,49 +1,17 @@
-function onload() {
+function onload(){
   console.log("Oh hi!");
 }
-//listen to shake event
-// function shakes() {
-  var shakeEvent = new Shake({threshold: 15});
-  shakeEvent.start();
-  window.addEventListener('shake', function() {
-    // $("#result").html(`<p><img width="24" src="img/loading.svg"/>Loading...</p>`)
+    //listen to shake event
+    var shakeEvent = new Shake({threshold: 15});
+    shakeEvent.start();
+    window.addEventListener('shake', function(){
+        alert("Shaked");
+    }, false);
 
-    // var city = document.getElementById('result').value;
+    //stop listening
+    function stopShake(){
+        shakeEvent.stop();
+    }
 
-    $.ajax({
-      url: "https://icanhazdadjoke.com/",
-      dataType: 'json'
-    }).done(function(res) {
-      renderResults(res);
-    }).fail(function(jq, status, err) {
-      showError(jq.status);
-    })
-
-  }, false);
-// }
-
-function renderResults(res) {
-  // $("p.heading").css("display", "none");
-  // $("#result").text(res.joke);
-  $(".heading").html(`<div class="heading row">
-      <div class="col-xs-12">
-        <p class="joke-text">${res.joke}</p>
-      </div>
-    </div>`);
-  $("#phone").html("");
-  $("#instruct").html(`<p>Do you like it?<br> Keep it shakin'</p>`)
-}
-
-function showError(status) {
-  alert("Error")
-}
-
-// stop listening
-function stopShake(){
-    shakeEvent.stop();
-}
-
-//check if shake is supported or not.
-if (!("ondevicemotion" in window)) {
-  alert("Not Supported");
-}
+    //check if shake is supported or not.
+    if(!("ondevicemotion" in window)){alert("Not Supported");}
