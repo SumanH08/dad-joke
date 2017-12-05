8e0a89e6f681d29c205afb432dbbe65282056ed2
Suman Hiremath <suman.hiremath89@gmail.com>
2017-12-05 20:54:42 +0530

    works for click and shake, based on gyro present or not

--- script.js
+++ script.js
@@ -6,19 +6,64 @@ function onload() {
 var shakeEvent = new Shake({
   threshold: 15
 });
-shakeEvent.start();
-window.addEventListener('shake', function() {
-  $.ajax({
-    url: "https://icanhazdadjoke.com/",
-    dataType: 'json'
-  }).done(function(res) {
-    renderResults(res);
-  }).fail(function(jq, status, err) {
-    showError(jq.status);
-  })
 
-}, false);
-// }
+var shakedCalled = false;
+
+var gyroPresent = false;
+window.addEventListener("devicemotion", function(event) {
+  console.log("Function executed");
+  if (event.rotationRate.alpha || event.rotationRate.beta || event.rotationRate.gamma) {
+
+    gyroPresent = true;
+    console.log("Disabling button now");
+    document.getElementById('joke-button').disabled = true;
+    document.getElementById('joke-button').style.display = "none";
+    window.removeEventListener("devicemotion", function(){
+      alert("De registered");
+    }, true);
+
+    if(!shakedCalled) {
+      getJokeOnShake();
+    }
+
+  }
+}, true);
+
+
+console.log("gyro status", gyroPresent);
+function getJokeOnShake(){
+  shakedCalled = true;
+  shakeEvent.start();
+  alert("Inside getJokeonshake func");
+  window.addEventListener('shake', function() {
+    alert("Sending request");
+    $.ajax({
+      url: "https://icanhazdadjoke.com/",
+      dataType: 'json'
+    }).done(function(res) {
+      renderResults(res);
+    }).fail(function(jq, status, err) {
+      showError(jq.status);
+    })
+
+  }, false);
+}
+
+
+function getJoke() {
+  var click_button = document.getElementById('joke-button');
+  click_button.addEventListener('click', function() {
+    $.ajax({
+      url: "https://icanhazdadjoke.com/",
+      dataType: 'json'
+    }).done(function(res) {
+      renderResults(res);
+    }).fail(function(jq, status, err) {
+      showError(jq.status);
+    })
+  }, false);
+
+}
 
 function renderResults(res) {
   var colors = ['#5D4037', '#BF360C', '#1B5E20', '#01579B', '#283593', '#4A148C', '#880E4F', '#004D40'];
